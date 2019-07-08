bb965b75868c76ac37d68ce43889a8501f5d2af8
gmzecmlibel <48735499+gmzecmlibel@users.noreply.github.com>
2019-07-08 11:03:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication32
+{
+    class Program
+    {
+        static List<string> sil(string s, char c)
+        {
+            List<string> gli = new List<string>();
+            List<string>li=new List<string>();
+            foreach (object o in li)
+            {
+                if (li.Contains(o))
+                    gli.Add(o.ToString());
+            }
+            return gli;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sil("adana", 'a'));
+        }
+    }
+}
